c1c66e9f159733927e31842b0f564807039f6e95
Jeremy Ward <jeremy2929@twc.com>
2016-12-08 22:40:26 -0600

    Tried JS loop for numbers 0 thru 9, no luck yet. Buttons 7,8, Clear and Decimal functional

--- 2.4-calculator/js/main.js
+++ 2.4-calculator/js/main.js
@@ -5,19 +5,44 @@
 /* creat even listener to wait until html fully loaded */
 document.addEventListener("DOMContentLoaded", function (){
 
- var numberButtonElement=document.querySelector("[data-js='numberButton']");
+var numberButtonElement7 = document.querySelector("[data-js='numberButton7']");
+var numberButtonElement8 = document.querySelector("[data-js='numberButton8']");
+var numberButtonElementClear = document.querySelector("[data-js='numberButtonClear']");
+var numberButtonElementDecimal = document.querySelector("[data-js='numberButtonDecimal']");
 
- var outputWindowJSElement = document.querySelector("[data-js='outputWindowJS']");
 
-    numberButtonElement.addEventListener("click", function(){
-      outputWindowJSElement.textContent=this.textContent
-      console.log(outputWindowJSElement);
+var outputTextElement = document.querySelector("[data-js='outputText']");
+
+numberButtonElementClear.addEventListener("click", function(){
+      outputTextElement.textContent = "";
+    })
+numberButtonElementDecimal.addEventListener("click", function(){
+      outputTextElement.textContent += ".";
     })
 
+numberButtonElement7.addEventListener("click", function(){
+      outputTextElement.textContent += this.textContent;
+    })
+
+numberButtonElement8.addEventListener("click", function(){
+          outputTextElement.textContent += this.textContent;
+        })
 
 
 
+  }
+)
+  /* var num = numberButtonElement[numberButtonElement.textContent];
 
+  numberButtonElement7.addEventListener("click", function(){
+    outputTextElement7.textContent += numberButtonElement[num].textContent;
+  })
+  numberButtonElement8.addEventListener("click", function(){
+    outputTextElement.textContent += numberButtonElement8.textContent;
+  {)
+  */
 
-
-})
+/*  numberButtonElement.addEventListener("click", function(){
+    var num = numberButtonElement.this.textContent;
+    console.log(num);
+    outputWindowJSElement[num].textContent=this.textContent */
