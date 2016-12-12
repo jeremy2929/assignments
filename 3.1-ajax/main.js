568ea39398f395526f0c806e999da16ac632e139
Jeremy Ward <jeremy2929@twc.com>
2016-12-12 16:57:53 -0600

    Laying out design of page, img ok, working on column headings

--- 3.1-ajax/main.js
+++ 3.1-ajax/main.js
@@ -10,13 +10,33 @@ document.addEventListener("DOMContentLoaded",function(){
     var xhrData = this.response;
     var JSONData = JSON.parse(xhrData);
     console.log(JSONData);
-    var lectureArticleHTML="<article>";
+    var lectureArticleHTML="<article class='article'>";
     lectureArticleHTML += "<img src='";
     lectureArticleHTML += JSONData.avatar_url;
     lectureArticleHTML += "' alt='avatar image' class='avatar'>";
-    lectureArticleHTML += "created: ";
-    lectureArticleHTML += JSONData.created_at;
+    lectureArticleHTML += "<div class=tipBox>";
+    lectureArticleHTML += "ProTip";
+
+    lectureArticleHTML += "</div>";
+
+
+    lectureArticleHTML += "<p class='title'>";
+    lectureArticleHTML += "Overviews    ";
+    lectureArticleHTML += "Repositories   ";
+    lectureArticleHTML += JSONData.public_repos;
+
+
+    lectureArticleHTML += "Stars   ";
+    lectureArticleHTML += "Followers   ";
+    lectureArticleHTML += JSONData.followers;
+
     lectureArticleHTML += "</p>";
+    lectureArticleHTML += "<p class='login_name'>";
+    lectureArticleHTML += JSONData.login;
+    lectureArticleHTML += "</p>";
+    lectureArticleHTML += JSONData.created_at;
+    lectureArticleHTML += "created: ";
+
 
     console.log ("Created", JSONData.created_at);
     console.log ("login", JSONData.login);
