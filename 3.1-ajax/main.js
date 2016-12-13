29e9bf8a719d3bbca1ca34663d250a89e0a68a30
Jeremy Ward <jeremy2929@twc.com>
2016-12-12 21:48:29 -0600

    Six values from github api being displayed, layout looks good, need comments

--- 3.1-ajax/main.js
+++ 3.1-ajax/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded",function(){
   var xhr = new XMLHttpRequest();
 
 
+
   xhr.open("GET", "https://api.github.com/users/jeremy2929");
   xhr.addEventListener("load", function(e){
 
@@ -15,26 +16,40 @@ document.addEventListener("DOMContentLoaded",function(){
     lectureArticleHTML += JSONData.avatar_url;
     lectureArticleHTML += "' alt='avatar image' class='avatar'>";
     lectureArticleHTML += "<div class=tipBox>";
+      lectureArticleHTML += "<button class='editProfile'>";
+        lectureArticleHTML += "Edit profile";
+      lectureArticleHTML += "</button>";
       lectureArticleHTML += "<p class='tipTitle'>";
-      lectureArticleHTML += "ProTip";
+        lectureArticleHTML += "ProTip!  Updating your profile with your name, location, and a profile picture helps other GitHub users get to know you.";
+
       lectureArticleHTML += "</p>";
       lectureArticleHTML += "<article1 class=titlesBox>";
-        lectureArticleHTML += "<p class='overviewsTitle'>";
-        lectureArticleHTML += "Overviews    ";
+        lectureArticleHTML += "<p class='overviewTitle'>";
+        lectureArticleHTML += "Overview    ";
         lectureArticleHTML += "</p>";
         lectureArticleHTML += "<p class='repositoriesTitle'>";
         lectureArticleHTML += "Repositories   ";
-        lectureArticleHTML += JSONData.public_repos;
+          lectureArticleHTML += "<span class='spanTitle'>";
+          lectureArticleHTML += JSONData.public_repos;
+          lectureArticleHTML += "</span>";
         lectureArticleHTML += "</p>";
         lectureArticleHTML += "<p class='starsTitle'>";
-        lectureArticleHTML += "Stars   ";
+          lectureArticleHTML += "Stars   ";
+          lectureArticleHTML += "<span class='spanTitle'>";
+          lectureArticleHTML += 0;
+          lectureArticleHTML += "</span>";
         lectureArticleHTML += "</p>";
         lectureArticleHTML += "<p class='followersTitle'>";
         lectureArticleHTML += "Followers   ";
-        lectureArticleHTML += JSONData.followers;
+          lectureArticleHTML += "<span class='spanTitle'>";
+          lectureArticleHTML += JSONData.followers;
+          lectureArticleHTML += "</span>";
         lectureArticleHTML += "</p>";
         lectureArticleHTML += "<p class='followingTitle'>";
         lectureArticleHTML += "Following   ";
+        lectureArticleHTML += "<span class='spanTitle'>";
+        lectureArticleHTML += JSONData.following;
+        lectureArticleHTML += "</span>";
         lectureArticleHTML += "</p>";
       lectureArticleHTML += "</artile1>";
     lectureArticleHTML += "</div>";
@@ -46,9 +61,22 @@ document.addEventListener("DOMContentLoaded",function(){
     lectureArticleHTML += "<p class='login_name'>";
     lectureArticleHTML += JSONData.login;
     lectureArticleHTML += "</p>";
-    lectureArticleHTML += JSONData.created_at;
-    lectureArticleHTML += "created: ";
 
+    lectureArticleHTML += "<p class='addBio'>";
+    lectureArticleHTML += "Add a Bio";
+    lectureArticleHTML += "</p>";
+    lectureArticleHTML += "<button class='editProfileLower'>";
+    lectureArticleHTML += "Edit profile";
+    lectureArticleHTML += "</button>";
+    var date  = new Date(JSONData.created_at);
+    var holdDate = date.toString()
+    var newDate = holdDate.substring(4,10) + ",";
+    newDate += holdDate.substring(10,16);
+/*    <local-time class="join-date" datetime="2016-11-19 04:11:18 UTC" day="numeric" month="short" year="numeric" title="Nov 18, 2016, 10:11 PM CST">Nov 18, 2016</local-time>
+*/
+    lectureArticleHTML += "<p class='createdDate'>";
+    lectureArticleHTML += "Joined on " + newDate;
+    lectureArticleHTML += "</p>";
 
     console.log ("Created", JSONData.created_at);
     console.log ("login", JSONData.login);
