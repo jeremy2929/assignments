9944ef4c7d384f2de65fdca6851d43df3ba9c62c
Jeremy Ward <jeremy2929@twc.com>
2016-12-13 16:54:55 -0600

    Working on loops and condition statements to create three different actions based on value TYPE

--- 3.2-form-builder/main.js
+++ 3.2-form-builder/main.js
@@ -3,33 +3,36 @@ document.addEventListener("DOMContentLoaded", function(){
   var allHTML = document.querySelector("[data-js='formData']");
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "http://json-data.herokuapp.com/forms");
-
   xhr.addEventListener("load", function(e){
     var xhrData = this.response;
     var JSONData = JSON.parse(xhrData);
-
     var pieceHTML ="";
-
-
     for (var i = 0; i < JSONData.length; i++){
+      var inputData = JSONData[i];
+      if (inputData.type === 'select'){
+        pieceHTML += '<select name="select">';
+      /* console.log("JSONData=[]", inputData.options[2]);  */
+        for (var x = 0; x < inputData.options.length; x++){
+          var optionData = inputData.options[x];
+          console.log("option=",optionData);
+          pieceHTML += `<option value='${optionData.value}'>${optionData.label}</option>`;
+        }
+      }
+        pieceHTML += "</select>";
+        console.log(pieceHTML);
+    }
+      /*  console.log("this is a select",JSONData[i]); */
 
 
-    console.log("Data=",JSONData[i]);
-
-      var pieceHTML = `
-        <li>
-          <p>Icon: ${JSONData[i].icon}_____
-          Id: ${JSONData[i].id}_____
-          Label: ${JSONData[i].label}_____ </p>
-        </li>
-      `;
+    /*  pieceHTML = `
+          <form>
+          <input Icon: ${JSONData[i].icon}
+                 Id: ${JSONData[i].id}
+          <label> ${JSONData[i].label} </label>
+        </form>
+      `; */
       allHTML.innerHTML += pieceHTML;
-      console.log(allHTML);
-             }
+
   });
-
-
-
-
   xhr.send();
-})
+});
