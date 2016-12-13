4a073436de0f4f63db03a6b985374dc2f8d2ca05
Jeremy Ward <jeremy2929@twc.com>
2016-12-13 14:42:44 -0600

    All data displaying to output screen- Array of 8 elements, 3 values in each

--- 3.2-form-builder/main.js
+++ 3.2-form-builder/main.js
@@ -1,18 +1,31 @@
 /*  */
 document.addEventListener("DOMContentLoaded", function(){
-  var formDataElement = document.querySelector("[data-js='formData']");
+  var allHTML = document.querySelector("[data-js='formData']");
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "http://json-data.herokuapp.com/forms");
 
   xhr.addEventListener("load", function(e){
     var xhrData = this.response;
     var JSONData = JSON.parse(xhrData);
+
+    var pieceHTML ="";
+
+
     for (var i = 0; i < JSONData.length; i++){
 
 
     console.log("Data=",JSONData[i]);
 
-    }
+      var pieceHTML = `
+        <li>
+          <p>Icon: ${JSONData[i].icon}_____
+          Id: ${JSONData[i].id}_____
+          Label: ${JSONData[i].label}_____ </p>
+        </li>
+      `;
+      allHTML.innerHTML += pieceHTML;
+      console.log(allHTML);
+             }
   });
 
 
