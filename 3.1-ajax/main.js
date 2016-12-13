d5d188a4b851270154cc5121f44ad4bb64bd5cd2
Jeremy Ward <jeremy2929@twc.com>
2016-12-12 22:46:30 -0600

    More comments added, code cleaned up

--- 3.1-ajax/main.js
+++ 3.1-ajax/main.js
@@ -1,90 +1,91 @@
-
+/* This script sends request to API of GitHub for user Jalien2929 home URL data & displays
+   6 different values: User icon, User name, Number of repositories, number of
+   Followers, number of Followings, and the Joined on Date. */
+/* Wait for document to load before running script */
 document.addEventListener("DOMContentLoaded",function(){
+  /* Declaring and assigning variable linked to HTML   */
   var githubSectionElement = document.querySelector("[data-js='github']");
+  /* Create instance of XHR object   */
   var xhr = new XMLHttpRequest();
-
-
-
+  /* Open a specific URL   */
   xhr.open("GET", "https://api.github.com/users/jeremy2929");
+  /* Listen for the Load event. The argument used is e because event is reserved keyword  */
   xhr.addEventListener("load", function(e){
-
+    /* 'this' is equal to XHR event. xhrData will store the string of data we need */
     var xhrData = this.response;
+    /* Converting the data string into JSON */
     var JSONData = JSON.parse(xhrData);
-    console.log(JSONData);
-    var lectureArticleHTML="<article class='article'>";
-    lectureArticleHTML += "<img src='";
-    lectureArticleHTML += JSONData.avatar_url;
-    lectureArticleHTML += "' alt='avatar image' class='avatar'>";
-    lectureArticleHTML += "<div class=tipBox>";
-      lectureArticleHTML += "<button class='editProfile'>";
-        lectureArticleHTML += "Edit profile";
-      lectureArticleHTML += "</button>";
-      lectureArticleHTML += "<p class='tipTitle'>";
-        lectureArticleHTML += "ProTip!  Updating your profile with your name, location, and a profile picture helps other GitHub users get to know you.";
-
-      lectureArticleHTML += "</p>";
-      lectureArticleHTML += "<article1 class=titlesBox>";
-        lectureArticleHTML += "<p class='overviewTitle'>";
-        lectureArticleHTML += "Overview    ";
-        lectureArticleHTML += "</p>";
-        lectureArticleHTML += "<p class='repositoriesTitle'>";
-        lectureArticleHTML += "Repositories   ";
-          lectureArticleHTML += "<span class='spanTitle'>";
-          lectureArticleHTML += JSONData.public_repos;
-          lectureArticleHTML += "</span>";
-        lectureArticleHTML += "</p>";
-        lectureArticleHTML += "<p class='starsTitle'>";
-          lectureArticleHTML += "Stars   ";
-          lectureArticleHTML += "<span class='spanTitle'>";
-          lectureArticleHTML += 0;
-          lectureArticleHTML += "</span>";
-        lectureArticleHTML += "</p>";
-        lectureArticleHTML += "<p class='followersTitle'>";
-        lectureArticleHTML += "Followers   ";
-          lectureArticleHTML += "<span class='spanTitle'>";
-          lectureArticleHTML += JSONData.followers;
-          lectureArticleHTML += "</span>";
-        lectureArticleHTML += "</p>";
-        lectureArticleHTML += "<p class='followingTitle'>";
-        lectureArticleHTML += "Following   ";
-        lectureArticleHTML += "<span class='spanTitle'>";
-        lectureArticleHTML += JSONData.following;
-        lectureArticleHTML += "</span>";
-        lectureArticleHTML += "</p>";
-      lectureArticleHTML += "</artile1>";
-    lectureArticleHTML += "</div>";
-
-
-
-
-
-    lectureArticleHTML += "<p class='login_name'>";
-    lectureArticleHTML += JSONData.login;
-    lectureArticleHTML += "</p>";
-
-    lectureArticleHTML += "<p class='addBio'>";
-    lectureArticleHTML += "Add a Bio";
-    lectureArticleHTML += "</p>";
-    lectureArticleHTML += "<button class='editProfileLower'>";
-    lectureArticleHTML += "Edit profile";
-    lectureArticleHTML += "</button>";
+    /* Declaring variable gitHubArticleHTML and assigning first value. We will build our
+       HTML by concatenating to gitHubArticleHTML        */
+    var gitHubArticleHTML="<article class='article'>";
+    gitHubArticleHTML += "<img src='";
+    /* Including value of User image from JSON data    */
+    gitHubArticleHTML += JSONData.avatar_url;
+    gitHubArticleHTML += "' alt='avatar image' class='avatar'>";
+    gitHubArticleHTML += "<div class=tipBox>";
+    gitHubArticleHTML += "<button class='editProfile'>";
+    gitHubArticleHTML += "Edit profile";
+    gitHubArticleHTML += "</button>";
+    gitHubArticleHTML += "<p class='tipTitle'>";
+    gitHubArticleHTML += "ProTip!  Updating your profile with your name, location, and a profile picture helps other GitHub users get to know you.";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<article1 class=titlesBox>";
+    gitHubArticleHTML += "<p class='overviewTitle'>";
+    gitHubArticleHTML += "Overview    ";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<p class='repositoriesTitle'>";
+    gitHubArticleHTML += "Repositories   ";
+    gitHubArticleHTML += "<span class='spanTitle'>";
+    /* Including value of number of repositories from JSON data    */
+    gitHubArticleHTML += JSONData.public_repos;
+    gitHubArticleHTML += "</span>";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<p class='starsTitle'>";
+    gitHubArticleHTML += "Stars   ";
+    gitHubArticleHTML += "<span class='spanTitle'>";
+    gitHubArticleHTML += 0;
+    gitHubArticleHTML += "</span>";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<p class='followersTitle'>";
+    gitHubArticleHTML += "Followers   ";
+    gitHubArticleHTML += "<span class='spanTitle'>";
+    /* Including value of number of Followers from JSON data    */
+    gitHubArticleHTML += JSONData.followers;
+    gitHubArticleHTML += "</span>";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<p class='followingTitle'>";
+    gitHubArticleHTML += "Following   ";
+    gitHubArticleHTML += "<span class='spanTitle'>";
+    /* Including value of number Following from JSON data    */
+    gitHubArticleHTML += JSONData.following;
+    gitHubArticleHTML += "</span>";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "</artile1>";
+    gitHubArticleHTML += "</div>";
+    gitHubArticleHTML += "<p class='loginName'>";
+    /* Including value of Username from JSON data    */
+    gitHubArticleHTML += JSONData.login;
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<p class='addBio'>";
+    gitHubArticleHTML += "Add a Bio";
+    gitHubArticleHTML += "</p>";
+    gitHubArticleHTML += "<button class='editProfileLower'>";
+    gitHubArticleHTML += "Edit profile";
+    gitHubArticleHTML += "</button>";
+    /* Including value Joined On Date from JSON data    */
     var date  = new Date(JSONData.created_at);
+    /* Converting date into a usable format */
     var holdDate = date.toString()
+    /* Grabbing some of date so can add comma after   */
     var newDate = holdDate.substring(4,10) + ",";
+    /* Adding on rest of date info after comma        */
     newDate += holdDate.substring(10,16);
-/*    <local-time class="join-date" datetime="2016-11-19 04:11:18 UTC" day="numeric" month="short" year="numeric" title="Nov 18, 2016, 10:11 PM CST">Nov 18, 2016</local-time>
-*/
-    lectureArticleHTML += "<p class='createdDate'>";
-    lectureArticleHTML += "Joined on " + newDate;
-    lectureArticleHTML += "</p>";
-
-    console.log ("Created", JSONData.created_at);
-    console.log ("login", JSONData.login);
-    console.log ("number of followers", JSONData.followers);
-    console.log ("Type", JSONData.type);
-    console.log ("Updated", JSONData.updated_at);
-    console.log ("URL", JSONData.url);
-    githubSectionElement.innerHTML +=lectureArticleHTML;
+    gitHubArticleHTML += "<p class='createdDate'>";
+    gitHubArticleHTML += "> Joined on " + newDate;
+    gitHubArticleHTML += "</p>";
+    /* Inserting our HTML string into the HTML   */
+    githubSectionElement.innerHTML +=gitHubArticleHTML;
   });
+  /* Sending request to API of GitHub     */
   xhr.send();
 })
