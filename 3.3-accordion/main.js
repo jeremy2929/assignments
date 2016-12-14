0202f1c9da51dc986635baa7af59529e11a96650
Jeremy Ward <jeremy2929@twc.com>
2016-12-14 15:18:54 -0600

    Changed quotes to something real, added some boxes to text, added Expand slowly and collapse quickly yo javascript

--- 3.3-accordion/main.js
+++ 3.3-accordion/main.js
@@ -3,8 +3,10 @@ $(function(){
   $quotesElements.on("click",function(e){
     var $clickedQuotes = $(this);
     console.log ($clickedQuotes);
+    $quotesElements.filter(".quoteExpand").animate({height: '20px'}, "fast");
     $quotesElements.filter(".quoteExpand").removeClass("quoteExpand");
     $clickedQuotes.addClass("quoteExpand");
+    $clickedQuotes.animate({height: '150px'}, "slow");
 
 
   });
