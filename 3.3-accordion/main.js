635ff2127831593698e96648386de58108f841e0
Jeremy Ward <jeremy2929@twc.com>
2016-12-14 19:55:28 -0600

    Improved layout and design of output display

--- 3.3-accordion/main.js
+++ 3.3-accordion/main.js
@@ -1,14 +1,20 @@
+/* This script links to Jquery to utilize call a library of functions
+   The script will display a list planes and if one is clicked, will
+   expand a window blow that plane to provide a description. If a second
+   plane is clicked, then the first description fades away and the new
+   description will expand */
+
+// this anonymous function defined by Jquery waits until document fully loads
 $(function(){
+  // declare a variable to link to HTML elements
   var $quotesElements = $("[data-js='quotes']");
+  // using the .on function of Jquery to perform action when a 'click' occurs
   $quotesElements.on("click",function(e){
+    // declaring a new variable and assigning what element was clicked
     var $clickedQuotes = $(this);
-    console.log ($clickedQuotes);
-    $quotesElements.filter(".quoteExpand").animate({height: '20px'}, "fast");
+    // using filter function to search for any previous 'quoteExpand' classes to remove them
     $quotesElements.filter(".quoteExpand").removeClass("quoteExpand");
+    // adding 'quoteExpand' class to the element clicked
     $clickedQuotes.addClass("quoteExpand");
-    $clickedQuotes.animate({height: '150px'}, "slow");
-
-
   });
-
 });
