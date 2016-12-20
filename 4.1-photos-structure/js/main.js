70535d8277df11a741f4a2643becf2aeda9f89f3
Jeremy Ward <jeremy2929@twc.com>
2016-12-20 16:49:07 -0600

    Click event now hiding first page and displaying Photo page but only for one thumbnail

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -3,12 +3,15 @@ var modal = require("./modal");
 
 $(function(){
   var $body = $("body");
-  var $indexElement = $body.find("[data-js='indexElement']");
-  var $modalElement = $body.find("[data-js='modalElement']");
+  var $indexElement = $body.find("[data-js='indexHTML']");
+  var $modalElement = $body.find("[data-js='modalHTML']");
   var $imageElement = $body.find("[data-js='imageHTML']");
   $imageElement.on("click",function(e){
 
-
+      $indexElement.filter(".indexPageShow").removeClass("indexPageShow");
+      $indexElement.addClass("indexPageNoShow");
+      $modalElement.filter(".photoNoPage").removeClass("photoNoPage");
+      $modalElement.addClass("photoPage");
   });
 
   });
