669ed836c311d46cedbfdb4a5377a813e351fda0
Jeremy Ward <jeremy2929@twc.com>
2016-12-20 16:21:32 -0600

    Click event created for image in javascript

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -1,2 +1,14 @@
 var $ = require("jquery");
 var modal = require("./modal");
+
+$(function(){
+  var $body = $("body");
+  var $indexElement = $body.find("[data-js='indexElement']");
+  var $modalElement = $body.find("[data-js='modalElement']");
+  var $imageElement = $body.find("[data-js='imageHTML']");
+  $imageElement.on("click",function(e){
+
+
+  });
+
+  });
