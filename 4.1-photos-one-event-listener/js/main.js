0c37e9ee054b8417950854696898a22cd64d9424
Jeremy Ward <jeremy2929@twc.com>
2016-12-25 18:16:19 -0600

    4.1 Photo Album with one event listner

--- 4.1-photos-one-event-listener/js/main.js
+++ 4.1-photos-one-event-listener/js/main.js
@@ -1,21 +1,35 @@
-var $ = require("jquery");
-var modal = require("./modal");
+var $=require("jquery");
+var modal=require("./modal");
 $(function(){
-  var $body = $("body");
-  var $indexElement = $body.find("[data-js='indexHTML']");
-  var $imageElement = $body.find("[data-js='imageHTML']");
-  var $articleElement=$body.find("[data-js='articleHTML']");
+  var $body=$("body");
+  var $indexElement=$body.find("[data-js='indexHTML']");
+  var $imageElement=$body.find("[data-js='imageHTML']");
   var $modalElement=$body.find("[data-js='modalHTML']");
+  var $titlePhotoElement=$body.find("[data-js='titlePhotoHTML']");
+  var $backButtonElement=$body.find("[data-js='backButton']");
+  var $photoImageElement=$body.find("[data-js='photoHTML']");
 
-  var $backButtonElement = $body.find("[data-js='backButton']");
-  var $photoImageElement = $body.find("[data-js='photoHTML']");
-  $indexElement.on("click",function(e){
-    console.log("clicked");
-      $indexElement.removeClass("indexPageShow");
-      $indexElement.addClass("indexPageNoShow");
-      $modalElement.removeClass("photoNoPage");
-      $modalElement.addClass("photoPage");
+
+  $imageElement.on("click",function(e){
+    var $clickedElement=$(this);
+    var srcImage=$clickedElement.attr("src");
+    $photoImageElement.attr("src",srcImage);
+
+    var altImage=$clickedElement.attr("alt");
+    $titlePhotoElement.text(altImage);
+
+
+  //  var altImage = $clickedImageElement.attr("alt");
+  //  $photoImageElement.attr("alt",altImage);
+
+
+    $indexElement.removeClass("indexPageShow");
+    $indexElement.addClass("indexPageNoShow");
+    $modalElement.removeClass("photoNoPage");
+    $modalElement.addClass("photoPage");
   });
+
+
   $backButtonElement.on("click",function(e){
       $indexElement.removeClass("indexPageNoShow");
       $indexElement.addClass("indexPageShow");
