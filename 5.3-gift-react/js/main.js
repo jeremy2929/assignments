cbbe14d4482acf4ae9a6aaa3cb2f932876ca17ec
Jeremy Ward <jeremy2929@twc.com>
2017-01-05 14:26:42 -0600

    boiler plate set up for 5.3

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+//document.addEventListener("DOMContentLoaded", function(){
 
 
 
   // Loads initial file(s)
-  var newName = require("./newFile");
+//  var newName = require("./newFile");
 
-  
-})
+
+//})
+
+import React from 'react'
