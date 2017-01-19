30efddc47c51b281b16243f234594f5ae582494d
Jeremy Ward <jeremy2929@twc.com>
2017-01-19 16:55:02 -0600

    Validating credit card now, need to clean up, meet MVP

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-
+var Card = require('react-credit-card')
+var creditCardValidator = require('credit-card-validator');
 
 export default React.createClass({
   componentDiDMount(){
@@ -9,21 +10,34 @@ export default React.createClass({
   },
   getInitialState(){
     return{
-        emailText: ""
+        cardsEntered: ""
     }
   },
-  onInputChange(e){
-
+  onCardNumber(e){
+      e.preventDefault()
+      var cardNum = e.target.value
+      console.log("card num=",cardNum);
+  //    var validCard = creditCardValidator.validateCardAndSecCode('4111 1111 1111 1111', '412');
+  var validCard = creditCardValidator.validateCard(cardNum)
+  console.log("valid? = ",validCard);
   },
+
+
   render() {
 
     return(
-      <section>
-        <form className={this.state.isEmailValid ? "valid" : "invalid"}>
-            <input type="cc" onChange={this.onInputChange}/>
-            <input type="submit" onSubmit={this.onSubmit}/>
-        </form>
-      </section>
-  )
-}
+      <div className="demo-container">
+        <div className="card-wrapper">
+
+          <div className="form-container active">
+            <form action="">
+              <input placeholder="Card number" type="tel" name="number" onChange={this.onCardNumber}/>
+              <input placeholder="Full name" type="text" name="name"/>
+              <input placeholder="MM/YY" type="tel" name="expiry"/>
+              <input placeholder="CVC" type="number" name="cvc"/>
+            </form>
+          </div>
+        </div>
+      </div>
+)}
 })
