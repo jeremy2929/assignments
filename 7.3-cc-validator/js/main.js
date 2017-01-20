bfc57a826ef25d2ef08fcdea7c444b41afaf7ad3
Jeremy Ward <jeremy2929@twc.com>
2017-01-20 06:56:13 -0600

    CC validation and adding CC info to object

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 var Card = require('react-credit-card')
 var creditCardValidator = require('credit-card-validator');
-
+// this number was randomly entered but worked! 3535453535353535
 export default React.createClass({
   getInitialState(){
     return{
       cc_cards: [
           {
+            number: "214276575634532",
+            name: "John Doe",
+            valid: "Bad CC Number!"
+          },
+          {
             number: "21423523534532",
-            name: "john crap",
+            name: "Mr Ripoff",
+            valid: "Bad CC Number!"
           }
         ]
       }
@@ -19,25 +25,27 @@ export default React.createClass({
       var cardNum = e.target.value
       var validCard = creditCardValidator.validateCard(cardNum)
       if (validCard){
-        console.log("newcard",cardNum);
         var newCard = {
           number: cardNum,
-          name: "new guy"
+          name: "New Guy",
+          valid: "Good CC Number!"
         }
-      //   this.setState({cardsEntered:cardNum})
-      //   this.setState(this.state.cc_cards.concat(cardNum))
-    //  setState(this.state.cardsEntered)
-
+        this.refs.ccNum.value = ""
         this.state.cc_cards.unshift(newCard)
-     //this.setState(this.state.posts)
-     // clearing all the input values
-    //    this.state.cardsEntered.unshift(cardNum)
-        var numberOfCards=this.state.cc_cards.length
-        for (var i=0; i<numberOfCards; i++){
-          console.log("cardsEntered=",this.state.cc_cards[i].number);
-          console.log("cardsEntered=",this.state.cc_cards[i].name);
-        }
+        this.setState(this.state.cc_cards)
       }
+      var cardNumLength = cardNum.length
+      if (cardNumLength > 16){
+        var newCard = {
+          number: cardNum,
+          name: "New Guy",
+          valid: "Bad CC Number!"
+        }
+        this.refs.ccNum.value = ""
+        this.state.cc_cards.unshift(newCard)
+        this.setState(this.state.cc_cards)
+      }
+      console.log(validCard);
 },
 
   render() {
@@ -48,9 +56,21 @@ export default React.createClass({
 
           <div className="form-container active">
             <form action="">
-              <input className= "ccNumber" placeholder="Card number" type="tel" name="number" onChange={this.onCardNumber}/>
-              <input className= "ccName" placeholder="Full name" type="text" name="name"/>
+              <input className="ccInput" placeholder="Card number 16 digits max" pattern="[0-9]{13,16}"  ref="ccNum" onChange={this.onCardNumber}/>
             </form>
+            <h1 className="ccData"> Credit Card List</h1>
+              <ul className="ccList">
+                {
+                  this.state.cc_cards.map ((ccnum,i)=>
+                  {
+                    return <li key={i}>
+                            <p className="ccNumber">Number: {ccnum.number}</p>
+                            <p className="ccName">Name: {ccnum.name}</p>
+                            <p ref="ccValid">Validation: {ccnum.valid}</p>
+                          </li>
+                  })
+                }
+          </ul>
           </div>
         </div>
       </div>
