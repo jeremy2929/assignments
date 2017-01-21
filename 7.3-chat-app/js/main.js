83a1c61c7475846e4fabcebd7e65427c86321606
Jeremy Ward <jeremy2929@twc.com>
2017-01-21 11:18:12 -0600

    AJAX integrated & now storing emails on server and displaying response

--- 7.3-chat-app/js/main.js
+++ 7.3-chat-app/js/main.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Validator from 'validator'
+import { ajax } from 'jquery'
 
 export default React.createClass({
+  componentDidMount(){
+    ajax({
+      url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test",
+      dataType: "json",
+      success: this.onInitialAjaxLoadSuccess,
+      error: this.onAjaxLoadError
+    })
+  },
   getInitialState(){
     return{
       isEmailValid: false,
@@ -9,143 +18,59 @@ export default React.createClass({
       emailAddresses: ["test@crap.com"]
     }
   },
-  onClickSubmit(e){
-    e.preventDefault()
-    var emailInputValue = this.refs.emailInput.value
-    var isEmail = Validator.isEmail(emailInputValue);
-    if (isEmail){this.state.emailAddresses.unshift(emailInputValue)}
-    this.setState(this.state.emailAddresses)
-  },
-  render() {
-    return(
-      <section>
-          {
-            this.state.emailAddresses.map((emailAddress, i)=>{
-              return <span key={i}> { emailAddress.currentEmailValue } </span>
-            })
-          }
-        <form className={this.state.isEmailValid ? "valid" : "invalid"}>
-          <input ref="emailInput" type="email"></input>
-          <input type="submit" onClick={this.onClickSubmit}/>
-        </form>
-        <h2>Entered Emails</h2>
-        {
-          this.state.emailAddresses.map((emailAddress, i)=>{
-            return <ul key={i}>
-              <p>  { emailAddress}</p>
-              </ul>
-          })
-        }
-      </section>
-    )
-  }
-})
-
-// ******************************
-// project
-/*
-{
-                 this.state.posts.map ((post,i)=>
-                 {
-                   var preview = post.postText.substring(0,80)+"..."
-                   return <li key={i}>
-                            <a href="#" >
-                              <h1 className="postTitle"
-                                  ref="postTitle"
-                                  onClick={ this.onClickPostTitle }>{i+1+".   "+post.postTitle}
-                              </h1>
-                            </a>
-                            <p className="postDate">{"  ("+post.postDate+")"}</p>
-                            <p className="previewPost">{preview}</p>
-                         </li>
-                 })
-               }
-// ******************************
-
-
-
-/*import React from 'react'
-import { ajax } from 'jquery'
-import Validator from 'validator'
-
-export default React.createClass({
-  componentDidMount(){
-
-    ajax({
-      url: "https://tiny-tiny.herokuapp.com/collections/fall16",
-      dataType: "json",
-      success: this.onInitialAjaxLoadSuccess,
-      error: this.onAjaxLoadError
-    })
-  },
-
-  getInitiatlState(){
-    return{
-      isEmailValid: false,
-      emailValue: "",
-      emailAddresses: []
-    }
-  },
-
   onInitialAjaxLoadSuccess(response){
     this.setState({
       emailAddresses: response
     })
   },
-
   onPostAjaxLoadSuccess(response){
     this.setState({
-      emailAdresses: this.state.emailAdresses.contact(response)
+      emailAddresses: this.state.emailAddresses.concat(response)
     })
   },
-
   onAjaxLoadError(response){
-  },
 
-  onEmailChange(e){
-    var currentEmailInputValue = e.target.value
-    var isEmail = Validator.isEmail(currentEmailInputValue)
-    this.setState({
-      isEmailValid: isEmail,
-      emailValue: currentEmailInputValue
-    })
   },
-
-  onSubmit(e){
-    console.log("This is submitting");
+  onClickSubmit(e){
     e.preventDefault()
-    var currentEmailValue = this.state.emailValue;
-    var isEmailValid = this.state.isEmailValid;
-
-    if(isEmailValid){
+    var emailInputValue = this.refs.emailInput.value
+    var isEmail = Validator.isEmail(emailInputValue);
+    if (isEmail){
+    //  this.state.emailAddresses.unshift(emailInputValue)
+    //  this.setState(this.state.emailAddresses)
+    //  console.log(this.state.emailAddresses);
       ajax({
-        url: "https://tiny-tiny.herokuapp.com/collections/fall16",
+        url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test",
         dataType: "json",
         type: "POST",
-        data: {
-            currentEmailValue
-        },
+        data:
+          {
+            emailInputValue
+          },
         success: this.onPostAjaxLoadSuccess,
-        error: this.onAjaxLoadError
+        error: this.AjaxLoadError
       })
     }
+    this.refs.emailInput.value=""
   },
   render() {
     return(
+<main>
       <section>
-        <h2>Current Emails</h2>
-          {
-            this.state.emailAddresses.map((emailAddress, i)=>{
-              return <span key={i}> { emailAddress.currentEmailValue } </span>
-            })
-          }
-        <form className={this.state.isEmailValid ? "valid" : "invalid"}
-          onSubmit={this.onSubmit}>
-          <input type="email" onChange={this.onEmailChange} />
-          <input type="submit" />
-        </form>
+        <input ref="emailInput" type="email"/>
+        <input type="submit" onClick={this.onClickSubmit}/>
       </section>
+      <section>
+        <h2>Entered Emails</h2>
+        {
+          this.state.emailAddresses.map((emailAddress, i)=>{
+            return <p key={i}>
+                      {emailAddress.emailInputValue}
+                   </p>
+          })
+        }
+      </section>
+</main>
     )
   }
 })
-*/
